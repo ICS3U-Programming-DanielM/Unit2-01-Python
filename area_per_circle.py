5a71a525d5cf635b37ba6b574d29d5ed89b1eb72
Daniel Momoh <daniel.momoh@stu.ocsb.ca>
2022-09-30 17:41:55 +0000

    Added area_per_circle.py

--- area_per_circle.py
+++ area_per_circle.py
@@ -2,14 +2,14 @@
 
 # Created by: Daniel Momoh
 # Created on: Sept. 21, 2022
-# This program calculates and displays the area and perimeter of a circle with radius 25cm.
+# This program calculates and displays the area and perimeter
+# of a circle with radius 25cm.
 import math
 
 def main():
     print ("For a circle that has a radius")
     print ("of 25cm:")
-    print ("")
-    print ("Area = {} cm^2". format (math.pi*25**2))
+    print ("")    print ("Area = {} cm^2". format (math.pi*25**2))
     print ("Perimeter = {} cm". format (2*math.pi*25))
 
 
